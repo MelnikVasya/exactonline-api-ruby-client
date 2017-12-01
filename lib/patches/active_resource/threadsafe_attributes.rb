de02fff2c77e8b0748d10d14accd351d079c944d
Max Hordiichuk <hordijchuk.m.i@gmail.com>
2017-12-01 14:30:26 +0200

    Fix "can't dup Symbol" bug

---
+++ lib/patches/active_resource/threadsafe_attributes.rb
@@ -0,0 +1,17 @@
+# Fix bug when trying to call #dup method on symbol value
+if Rails::VERSION::MAJOR <= 4
+  module ThreadsafeAttributes
+    private
+
+    def get_threadsafe_attribute(name, main_thread)
+      if threadsafe_attribute_defined_by_thread?(name, Thread.current)
+        get_threadsafe_attribute_by_thread(name, Thread.current)
+      elsif threadsafe_attribute_defined_by_thread?(name, main_thread)
+        value = get_threadsafe_attribute_by_thread(name, main_thread)
+        value = value.dup if value.duplicable?
+        set_threadsafe_attribute_by_thread(name, value, Thread.current)
+        value
+      end
+    end
+  end
+end
